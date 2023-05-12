8c1dd30e1a2d08eba51d9199cb31235aab530fb8
Felipe Kenji Yunoki <87314665+FelipeYunoki@users.noreply.github.com>
2023-05-12 12:28:26 -0300

    adicionando o array

--- Views-Resources-Laravel/routes/web.php
+++ Views-Resources-Laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', 'ProductController');
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+
+Route::get('/product/{id}', [ProductController::class, 'show'])->where('id', '[0-9]{1,5}')->name('product.show');
+
+Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->where('id', '[0-9]{1,5}')->name('product.edit');
+
+Route::put('/product/{id}', [ProductController::class, 'update'])->where('id', '[0-9]{1,5}')->name('product.update');
