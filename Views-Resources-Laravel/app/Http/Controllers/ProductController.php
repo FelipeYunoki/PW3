47f724599b407c7ec96f0c623533f83f96f9dccd
Felipe Kenji Yunoki <87314665+FelipeYunoki@users.noreply.github.com>
2023-05-12 12:03:51 -0300

    Implementar os métodos no controller

--- Views-Resources-Laravel/app/Http/Controllers/ProductController.php
+++ Views-Resources-Laravel/app/Http/Controllers/ProductController.php
@@ -12,7 +12,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return view('products.index', compact('products'));
     }
 
     /**
@@ -20,7 +21,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view('products.create');
     }
 
     /**
@@ -28,7 +29,18 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validação dos campos
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'description' => 'required',
+        ]);
+
+        // Criação do produto
+        $product = Product::create($validatedData);
+
+        return redirect()->route('products.show', $product->id)
+            ->with('success', 'Produto criado com sucesso!');
     }
 
     /**
@@ -36,7 +48,8 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view('products.show', compact('product'));
     }
 
     /**
@@ -44,7 +57,8 @@ class ProductController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view('products.edit', compact('product'));
     }
 
     /**
@@ -52,7 +66,19 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // Validação dos campos
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'description' => 'required',
+        ]);
+
+        // Atualização do produto
+        $product = Product::findOrFail($id);
+        $product->update($validatedData);
+
+        return redirect()->route('products.show', $product->id)
+            ->with('success', 'Produto atualizado com sucesso!');
     }
 
     /**
@@ -60,6 +86,10 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return redirect()->route('products.index')
+            ->with('success', 'Produto apagado com sucesso!');
     }
 }
